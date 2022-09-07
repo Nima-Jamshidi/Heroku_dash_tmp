65b4955c7156115bd97ed48efba8d69d234be0d2
Nima-Jamshidi <anima.djamshidi@gmail.com>
2022-09-07 18:18:01 -0400

    Trying to stick to binary

--- init.R
+++ init.R
@@ -9,10 +9,11 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
+options(install.packages.check.source = "no")
 install.packages('remotes')
 
 install.packages(c('jsonlite', 'curl', 'triebeard', 'Rcpp', 'magrittr', 'rlang', 'later', 'R6', 'fastmap', 'base64enc', 'digest', 'lazyeval', 'htmltools', 'utf8', 'cli', 'glue', 'vctrs', 'pkgconfig', 'pillar', 'lifecycle', 'fansi', 'purrr', 'ellipsis', 'tidyselect', 'tibble', 'generics', 'cpp11', 'dplyr', 'yaml', 'sys', 'askpass', 'openssl', 'mime', 'colorspace', 'viridisLite', 'RColorBrewer', 'munsell', 'labeling', 'farver', 'withr', 'scales', 'isoband', 'gtable', 'webutils', 'xml2', 'brotli', 'urltools', 'stringi', 'assertthat', 'parallelly', 'listenv', 'globals', 'promises', 'data.table', 'crosstalk', 'tidyr', 'htmlwidgets', 'httr', 'ggplot2', 'reqres', 'uuid', 'crayon', 'future', 'httpuv', 'plotly', 'routr', 'fiery'))
-install.packages(c('bit', 'ps', 'sass', 'cachem', 'memoise', 'rappdirs', 'rematch', 'bit64', 'prettyunits', 'processx', 'evaluate', 'highr', 'xfun', 'bslib', 'jquerylib', 'tinytex', 'backports', 'blob', 'DBI', 'gargle', 'cellranger', 'ids', 'rematch2', 'clipr', 'vroom', 'tzdb', 'progress', 'callr', 'fs', 'knitr', 'rmarkdown', 'selectr', 'broom', 'dbplyr', 'dtplyr', 'forcats', 'googledrive', 'googlesheets4', 'haven', 'hms', 'lubridate', 'modelr', 'readr', 'readxl', 'reprex', 'rstudioapi', 'rvest', 'stringr'))
+install.packages(c('tidyverse','bit', 'ps', 'sass', 'cachem', 'memoise', 'rappdirs', 'rematch', 'bit64', 'prettyunits', 'processx', 'evaluate', 'highr', 'xfun', 'bslib', 'jquerylib', 'tinytex', 'backports', 'blob', 'DBI', 'gargle', 'cellranger', 'ids', 'rematch2', 'clipr', 'vroom', 'tzdb', 'progress', 'callr', 'fs', 'knitr', 'rmarkdown', 'selectr', 'broom', 'dbplyr', 'dtplyr', 'forcats', 'googledrive', 'googlesheets4', 'haven', 'hms', 'lubridate', 'modelr', 'readr', 'readxl', 'reprex', 'rstudioapi', 'rvest', 'stringr'))
 remotes::install_github('plotly/dashR')
 # remotes::install_github('cran/dash', upgrade=TRUE)
 # install.packages("dash")
@@ -22,7 +23,7 @@ remotes::install_github('facultyai/dash-bootstrap-components@r-release')
 
 # install.packages('dashCoreComponents')
 # install.packages('dashHtmlComponents')
-install.packages('tidyverse')
+# install.packages('tidyverse')
 # install.packages('plotly')
 # install.packages('gapminder')
 # install.packages('base64enc')
