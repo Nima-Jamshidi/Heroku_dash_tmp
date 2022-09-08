30a3dbfa53cc4be669c49cabd160ff6f735e47c0
Nima-Jamshidi <anima.djamshidi@gmail.com>
2022-09-08 12:11:27 -0400

    change run scheme

--- app.R
+++ app.R
@@ -374,9 +374,9 @@ app$callback(
   }
 )
 
-# app$run_server(host = '0.0.0.0', port = Sys.getenv('PORT', 8050))
+app$run_server(host = '0.0.0.0', port = Sys.getenv('PORT', 8050))
 
-app$run_server(debug=TRUE)
+# app$run_server(debug=TRUE)
 # app %>% run_app(host = '0.0.0.0', port = Sys.getenv('PORT'))
 # app %>% run_app(
 #   host = Sys.getenv("DASH_HOST", Sys.getenv("HOST", "0.0.0.0")),
